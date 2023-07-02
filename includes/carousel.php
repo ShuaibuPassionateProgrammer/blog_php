7100b2622086c980b34fee0ee0144527c44d64ae
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-02 10:39:27 +0100

    Done with adding the slide

--- includes/carousel.php
+++ includes/carousel.php
@@ -21,10 +21,33 @@
         </div>
         <div class="carousel-item">
             <div class="view">
-                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg" class="d-block w-100" >
-                <div class="mask rgba-black-strong"></div>
-                <p>Secondary text</p>
+                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg" alt="First slide" class="d-block w-100">
+                <div class="mask rgba-black-light"></div>
+            </div>
+            <div class="carousel-caption">
+                <h3 class="h3-responsive">Light mask</h3>
+                <p>First text</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="view">
+                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).jpg" alt="First slide" class="d-block w-100">
+                <div class="mask rgba-black-light"></div>
+            </div>
+            <div class="carousel-caption">
+                <h3 class="h3-responsive">Light mask</h3>
+                <p>First text</p>
             </div>
         </div>
     </div>
+
+    <!-- Controls -->
+    <a href="#carousel-example-2" role="button" data-slide="prev" class="carousel-control-prev">
+        <span class="carousel-control-prev-icon" aria-hidden=true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a href="#carousel-example-2" role="button" data-slide="next" class="carousel-control-prev">
+        <span class="carousel-control-next-icon" aria-hidden=true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
