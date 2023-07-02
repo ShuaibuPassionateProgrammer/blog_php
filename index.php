69da1be0fd248b96de224754f3a45719719e47da
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-02 10:55:18 +0100

    The first card column is added

--- index.php
+++ index.php
@@ -4,4 +4,16 @@
 
 <?php include("includes/carousel.php"); ?>
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h3>Hello, Friends</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php include("include/footer.php"); ?>
