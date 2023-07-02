dd7ce9c00563920dede8b4fb9e7fa00ac6fe070a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-02 10:29:45 +0100

    Adding the carousel context

---
+++ includes/carousel.php
@@ -0,0 +1,30 @@
+<!-- Carousel Wrapper -->
+<div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+    <!-- Indicators -->
+     <ol class="carousel-indicators">
+        <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-2" data-slide-to="1"></li>
+        <li data-target="#carousel-example-2" data-slide-to="2"></li>
+     </ol>
+
+    <!-- Slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <div class="view">
+                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg" alt="First slide" class="d-block w-100">
+                <div class="mask rgba-black-light"></div>
+            </div>
+            <div class="carousel-caption">
+                <h3 class="h3-responsive">Light mask</h3>
+                <p>First text</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="view">
+                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg" class="d-block w-100" >
+                <div class="mask rgba-black-strong"></div>
+                <p>Secondary text</p>
+            </div>
+        </div>
+    </div>
+</div>
