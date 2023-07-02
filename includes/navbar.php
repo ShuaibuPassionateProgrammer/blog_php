189f7a2323e3e69f5a07e56f8bd3481472234905
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-02 09:20:51 +0100

    The Nav Links is created

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,7 +5,31 @@
     <a href="#" class="navbar-brand">Navbar</a>
 
     <!-- Collapse button -->
-     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav" aria-controls="myNav" aria-expanded="false" aria-label="Toggle Navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav" aria-controls="myNav" aria-expanded="false" aria-label="Toggle Navigation">
         <span class="navbar-toggler-icon"></span>
-     </button>
+    </button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="myNav">
+        <!-- Nav Links -->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a href="#" class="nav-link">Home
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">Home
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">Home
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+        </ul>
+    </div>
  </nav>
