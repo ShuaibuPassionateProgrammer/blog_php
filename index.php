585b30c36230d46a390f6b7b89d0f9c4e3bb4cdc
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-02 10:57:48 +0100

    The a proper padding is adding the main wrapper (root container)

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <?php include("includes/carousel.php"); ?>
 
-<div class="container">
+<div class="container py-5">
     <div class="row">
         <div class="col-md-4">
             <div class="card">
