3861154cd4a3ef6274290d771ac20fbb07d6419c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-02 09:44:46 +0100

    The navbar file is created

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,7 +5,7 @@
     <a href="#" class="navbar-brand">MyCollege Website</a>
 
     <!-- Collapse button -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav" aria-controls="myNav" aria-expanded="false" aria-label="Toggle Navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNav" aria-controls="myNav" aria-expanded="false" aria-label="Toggle Navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
