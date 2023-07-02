61a9f7877474c5f2d0776e7d9647352b193740e1
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-02 09:11:14 +0100

    The collapse button is created

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,4 +3,9 @@
 
     <!-- Navbar brand  -->
     <a href="#" class="navbar-brand">Navbar</a>
+
+    <!-- Collapse button -->
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav" aria-controls="myNav" aria-expanded="false" aria-label="Toggle Navigation">
+        <span class="navbar-toggler-icon"></span>
+     </button>
  </nav>
