dcd3857a42890e970bbff6c4c1fe2b94b2206f11
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-03 01:25:27 +0100

    The first card inside the about file is generated

--- abouts.php
+++ abouts.php
@@ -2,6 +2,20 @@
 
 <?php include("includes/navbar.php"); ?>
 
-This is About Us page
+<div class="container py-5">
+    <div class="row py-3">
+        <div class="col-md-8">
+
+        <div class="card">
+            <img src="#" alt="..." class="card-img-top">
+            <div class="card-body">
+                <div class="card-title">About My Website</div>
+                <div class="card-text">Some quick example text to build on the card title and make up the bulk on...</div>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
 
 <?php include("include/footer.php"); ?>
