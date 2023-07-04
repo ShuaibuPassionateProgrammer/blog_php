80d779b47bd7219dbee66a6312c627cdbfa06e0a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-04 15:22:40 +0100

    An input field is added

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,11 @@
 <?php include("includes/header.php"); ?>
     <div class="container">
         <div class="row justify-content-center">
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>"></form>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <div class="mb-3">
+                    <input type="text" class="input form-control" placeholder="Username">
+                </div>
+            </form>
         </div>
     </div>
 
