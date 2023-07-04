4d9464d0f1edfb696336cf4927aa4bef9f6ee0c8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-04 15:18:13 +0100

    The header file is included inside the Admin file

--- admin/login.php
+++ admin/login.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login</title>
-</head>
-<body>
+<?php include("includes/header.php"); ?>
     <div class="container">
         <div class="row justify-content-center">
             <button>Back to Business</button>
