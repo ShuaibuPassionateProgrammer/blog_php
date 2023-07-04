d43326fae6bb88327c4f29e0ebd74b2a1c467460
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-04 15:43:09 +0100

    The input field is correctly designed

--- admin/login.php
+++ admin/login.php
@@ -1,8 +1,11 @@
 <?php include("includes/header.php"); ?>
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center mt-4">
             <div class="col-md-6">
                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                    <div class="mb-3 text-center">
+                        <h2>BLOG Admin</h2>
+                    </div>
                     <div class="mb-3">
                         <input type="text" class="input form-control" placeholder="Username">
                     </div>
