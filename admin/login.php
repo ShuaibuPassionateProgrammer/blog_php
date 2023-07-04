b3566bd4f6a3a525b83b143f425894e1b04c58d4
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-04 15:24:37 +0100

    The is moved to the column

--- admin/login.php
+++ admin/login.php
@@ -1,11 +1,13 @@
 <?php include("includes/header.php"); ?>
     <div class="container">
         <div class="row justify-content-center">
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                <div class="mb-3">
-                    <input type="text" class="input form-control" placeholder="Username">
-                </div>
-            </form>
+            <div class="col-md-6">
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                    <div class="mb-3">
+                        <input type="text" class="input form-control" placeholder="Username">
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 
