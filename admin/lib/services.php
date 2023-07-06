f6103665ed6358cd56a3b62a2f51de2eeef67d02
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-06 15:16:03 +0100

    Get a form data

--- admin/lib/services.php
+++ admin/lib/services.php
@@ -5,7 +5,10 @@ $email = "";
 $password = "";
 $errors = [];
 
-if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['login'])) {}
+if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['login'])) {
+    $email = mysqli_real_escape_string($connection, $_POST['email']);
+    $password = mysqli_real_escape_string($connection, $_POST['password']);
+}
 else {
     header("Location: ../");
 }
