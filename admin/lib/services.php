089368aadbfc9a347170294e3868a95f77f1c583
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-06 15:38:28 +0100

    Redirect to the dasboard

--- admin/lib/services.php
+++ admin/lib/services.php
@@ -29,7 +29,11 @@ if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['login'])) {
 
             if($user && password_verify($password, $user['password'])) {
                 $_SESSION['email'] = $user['email'];
+
                 session_regenerate_id();
+
+                header("Location: ../dashboard.php");
+                exit();
             }
         }
     }
