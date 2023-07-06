e9797479a532d058a6b156d60a97e9654e8869f3
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-06 15:57:57 +0100

    Display the error

--- admin/errors.php
+++ admin/errors.php
@@ -1,7 +1,11 @@
 <?php
 if(count($errors) > 0) {
     echo "<div style=\"background-color: red; color: white; padding: 10px; text-align: center\">";
-
+        foreach($errors as $error) {
+            echo "<p>";
+                echo $error;
+            echo "</p>";
+        }
     echo "</div>";
 }
 ?>
