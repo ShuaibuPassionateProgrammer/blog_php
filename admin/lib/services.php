d39cb31cc90d2c518d660e004dbb450a6f55b2b7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-06 15:06:56 +0100

    Checks to see if the admin user has submitted the form and ensure that the login button is clicked

--- admin/lib/services.php
+++ admin/lib/services.php
@@ -4,3 +4,8 @@ require("../config/dbconfig.php");
 $email = "";
 $password = "";
 $errors = [];
+
+if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['login'])) {}
+else {
+    header("Location: ../");
+}
