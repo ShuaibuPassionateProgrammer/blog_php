386eca98759bd1ee648b037278b012106b26bd5b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-06 15:20:05 +0100

    Validate the email address

--- admin/lib/services.php
+++ admin/lib/services.php
@@ -8,6 +8,13 @@ $errors = [];
 if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['login'])) {
     $email = mysqli_real_escape_string($connection, $_POST['email']);
     $password = mysqli_real_escape_string($connection, $_POST['password']);
+
+    if(empty($email)) {
+        array_push($errors, "Email Address should not be empty!");
+    }
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($errors, "Enter a valid email address.");
+    }
 }
 else {
     header("Location: ../");
