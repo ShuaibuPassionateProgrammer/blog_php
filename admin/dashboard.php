79bf2a740cb212cbd0ae40348e2ac8b354bf207b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-07 17:09:55 +0100

    The Google Fonts Roboto is added to the dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -17,6 +17,11 @@ if(!isset($_SESSION['email'])) {
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
     />
+    <!-- Google Fonts Roboto -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+    />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
