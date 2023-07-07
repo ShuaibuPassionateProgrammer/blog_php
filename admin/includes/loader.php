5d5c8275c4a4149c94156896da70556b2f273e76
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-07 17:25:49 +0100

    CSS best design approac

--- admin/includes/loader.php
+++ admin/includes/loader.php
@@ -4,7 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Website</title>
-    <style></style>
+    <style>
+        * {
+            margin: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+    </style>
 </head>
 <body>
     <div class="loader">
