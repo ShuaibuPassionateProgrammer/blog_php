a4c66a0f5b46b04cd488cfb0e9bc3357c5caad8c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-07-07 16:58:52 +0100

    Attempting to fix the bug in errors file

--- admin/errors.php
+++ admin/errors.php
@@ -1,6 +1,6 @@
 <?php
 if(count($errors) > 0) {
-    echo "<div style=\"background-color: red; color: white; padding: 10px; text-align: center\">";
+    echo "<div style='background-color: red; color: white; padding: 10px; text-align: center'>";
         foreach($errors as $error) {
             echo "<p>";
                 echo $error;
