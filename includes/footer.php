467f4cdb034cf57388c2eb6912890e9611a4e8ea
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-07 17:51:45 +0100

    Footer page is improved

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,6 @@
-    
-
     <!-- MDB -->
     <script type="text/javascript" src="js/mdb.umd.min.js"></script>
     <!-- Custom scripts -->
-    <script type="text/javascript"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
   </body>
 </html>
