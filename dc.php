f26d7d1a219b93f236748834e3a005a1bc4e2169
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-07 17:46:46 +0100

    Department of Commerce page is improved

--- dc.php
+++ dc.php
@@ -1,9 +1,13 @@
-<?php include("includes/loader.php"); ?>
+<?php 
+$departmentName = "Commerce";
+include("includes/loader.php");
+include("includes/header.php");
+include("includes/navbar.php"); 
+?>
 
-<?php include("includes/header.php"); ?>
+<div class="container py-5">
+    <h1>Department of <?= htmlspecialchars($departmentName) ?></h1>
+    <p>This is the Department of <?= htmlspecialchars($departmentName) ?> page.</p>
+</div>
 
-<?php include("includes/navbar.php"); ?>
-
-This is Department of Commerce page
-
-<?php include("include/footer.php"); ?>
+<?php include("includes/footer.php"); ?>
