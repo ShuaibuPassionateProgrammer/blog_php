ce51cb1a2c94323a752740a15e724b2007c4b139
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-07 20:40:46 +0100

    Logout improved v1

--- admin/logout.php
+++ admin/logout.php
@@ -2,15 +2,12 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Basic CSRF token check
     if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
         die('Invalid CSRF token.');
     }
 
-    // Destroy session
     session_unset();
     session_destroy();
-
     header("Location: ./login.php");
     exit();
 } else {
