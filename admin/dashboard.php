2d9029e7327049111d2b9d5851a5ceef04002eab
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-07 21:03:28 +0100

    Using the custom style

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -26,6 +26,10 @@ if (empty($_SESSION['csrf_token'])) {
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+
+    <style>
+        //
+    </style>
 </head>
 <body>
     //
