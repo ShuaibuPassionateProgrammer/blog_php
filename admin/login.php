a4db571844801f7b6da2eef49af98e893a0de5c4
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-07 21:58:06 +0100

    Store and get the form data

--- admin/login.php
+++ admin/login.php
@@ -5,5 +5,8 @@ include("includes/header.php");
 
 $errors = [];
 
-if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['login'])) {}
+if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['login'])) {
+    $email = trim($_POST['email'] ?? '');
+    $password = $_POST['password'] ?? '';
+}
 ?>
