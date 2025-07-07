afacf3e8cd80c2c5ba440b29a2a72991e547969a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-07 22:01:33 +0100

    Proper email validation

--- admin/login.php
+++ admin/login.php
@@ -8,5 +8,11 @@ $errors = [];
 if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['login'])) {
     $email = trim($_POST['email'] ?? '');
     $password = $_POST['password'] ?? '';
+
+    if(empty($email)) {
+        $errors[] = "Email is required.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email format.";
+    }
 }
 ?>
