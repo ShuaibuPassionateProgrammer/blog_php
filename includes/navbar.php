881afcb31858bb4b5c6c98330d7d2104dc8afd9f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-07 17:51:13 +0100

    Navbar page is improved

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,48 +1,36 @@
 <!-- Navbar -->
- <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary primary-color">
-
-    <!-- Navbar brand  -->
-    <a href="#" class="navbar-brand">MyCollege Website</a>
-
-    <!-- Collapse button -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav" aria-controls="myNav" aria-expanded="false" aria-label="Toggle Navigation">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary primary-color">
+    <a href="index.php" class="navbar-brand">MyCollege Website</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav" aria-controls="myNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <!-- Collapsible content -->
     <div class="collapse navbar-collapse" id="myNav">
-        <!-- Nav Links -->
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a href="index.php" class="nav-link">Home
-                    <span class="sr-only">(current)</span>
-                </a>
+                <a href="index.php" class="nav-link">Home <span class="sr-only">(current)</span></a>
             </li>
-
             <li class="nav-item">
-                <a href="abouts.php" class="nav-link">About Us</a>
+                <a href="about.php" class="nav-link">About Us</a>
             </li>
-
             <li class="nav-item">
                 <a href="faculties.php" class="nav-link">Faculties</a>
             </li>
 
-            <!-- Dropdown -->
             <li class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Academics</a>
                 <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-                    <a href="#" class="dropdown-item">Department of Computer Science</a>
-                    <a href="#" class="dropdown-item">Department of Commerce</a>
-                    <a href="#" class="dropdown-item">Department of Arts</a>
+                    <a href="dcs.php" class="dropdown-item">Department of Computer Science</a>
+                    <a href="dc.php" class="dropdown-item">Department of Commerce</a>
+                    <a href="da.php" class="dropdown-item">Department of Arts</a>
                 </div>
             </li>
         </ul>
 
-        <form class="form-inline">
+        <form class="form-inline" action="search.php" method="get">
             <div class="md-form my-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                <input class="form-control mr-sm-2" name="query" type="text" placeholder="Search" aria-label="Search" />
             </div>
         </form>
     </div>
-
- </nav>
+</nav>
